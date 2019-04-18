f583be5462510750cad7a98aa4869109d78652ca
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-04-18 16:55:17 +0300

    layers maybe fixed!

--- flower_classification.py
+++ flower_classification.py
@@ -3,6 +3,9 @@ from keras import layers
 from keras import models
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+import sys
+from PIL import Image
+sys.modules['Image'] = Image
 
 
 def plt_modle(model_hist):
@@ -48,12 +51,42 @@ valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_
 # Model
 
 model = models.Sequential()
-model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='valid', data_format='channels_last'))
+
+model.add(keras.layers.Conv2D(64,kernel_size=(5,5), padding='same', input_shape=(128,128,3), activation='relu'))
+model.add(keras.layers.Conv2D(80,kernel_size=(3,3), padding='same', activation='relu'))
+model.add(keras.layers.Dropout(0.25))
+model.add(keras.layers.Conv2D(128, kernel_size=(7, 7), padding='same', activation='relu'))
+model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.25))
+model.add(keras.layers.Dense(300,activation='relu'))
+
+'''
+model.add(Conv1D(60,32, strides=1, activation='relu',padding='causal',input_shape=(None,64,1)))
+model.add(Conv1D(80,10, strides=1, activation='relu',padding='causal'))
+model.add(Dropout(0.25))
+model.add(Conv1D(100,5, strides=1, activation='relu',padding='causal'))
+model.add(MaxPooling1D(1))
+model.add(Dropout(0.25))
+model.add(Dense(300,activation='relu'))
+model.add(Dense(1,activation='relu'))
+'''
+'''
+model.add(keras.layers.Conv1D(60,32, strides=1, activation='relu',padding='causal',input_shape=(img_size, img_size, 3)))
+model.add(keras.layers.Conv1D(80,10, strides=1, activation='relu',padding='causal'))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.25))
+#model.add(keras.layers.Conv1D(100,5, strides=1, activation='relu',padding='causal'))
+#model.add(keras.layers.MaxPooling1D(1))
+#model.add(keras.layers.Dropout(0.25))
+'''
+'''
+model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='valid', data_format='channels_last', input_shape=(128,128,3)))
 filters = 20
 kernel_size = 3
 model.add(keras.layers.Conv1D(filters, kernel_size, strides=1, padding='valid', data_format='channels_last', dilation_rate=1, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None))
 #model.add(keras.layers.Conv2D(filters, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None))
-
+'''
 # use model.add() to add any layers you like
 # read Keras documentation to find which layers you can use:
 #           https://keras.io/layers/core/
@@ -63,7 +96,7 @@ model.add(keras.layers.Conv1D(filters, kernel_size, strides=1, padding='valid', 
 
 # last layer should be with softmax activation function - do not change!!!
 model.add(layers.Dense(classes, activation='softmax'))
-
+print(model.summary())
 # fill optimizer argument using one of keras.optimizers.
 # read Keras documentation : https://keras.io/models/model/
 optimizer = 'sgd'
