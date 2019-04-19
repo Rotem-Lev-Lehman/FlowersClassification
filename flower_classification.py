0232b09dd1caaa8ed57929c7ca75f02048e125c1
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-04-19 18:31:28 +0300

    model separation completed, now everyone can choose their model structure and see the results...

--- flower_classification.py
+++ flower_classification.py
@@ -52,50 +52,12 @@ valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_
 
 model = models.Sequential()
 
-model.add(keras.layers.Conv2D(64,kernel_size=(5,5), padding='same', input_shape=(128,128,3), activation='relu'))
-model.add(keras.layers.Conv2D(80,kernel_size=(3,3), padding='same', activation='relu'))
-model.add(keras.layers.Dropout(0.25))
-model.add(keras.layers.Conv2D(128, kernel_size=(7, 7), padding='same', activation='relu'))
-model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dropout(0.25))
-model.add(keras.layers.Dense(300,activation='relu'))
+# you can change the 'from' parameter to the file you are using your model on:
+# example, change to from model_<my name>_<my index> import initializeModel
+from model_rotem_2 import initializeModel, getName
+initializeModel(model, img_size, classes)
 
-'''
-model.add(Conv1D(60,32, strides=1, activation='relu',padding='causal',input_shape=(None,64,1)))
-model.add(Conv1D(80,10, strides=1, activation='relu',padding='causal'))
-model.add(Dropout(0.25))
-model.add(Conv1D(100,5, strides=1, activation='relu',padding='causal'))
-model.add(MaxPooling1D(1))
-model.add(Dropout(0.25))
-model.add(Dense(300,activation='relu'))
-model.add(Dense(1,activation='relu'))
-'''
-'''
-model.add(keras.layers.Conv1D(60,32, strides=1, activation='relu',padding='causal',input_shape=(img_size, img_size, 3)))
-model.add(keras.layers.Conv1D(80,10, strides=1, activation='relu',padding='causal'))
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dropout(0.25))
-#model.add(keras.layers.Conv1D(100,5, strides=1, activation='relu',padding='causal'))
-#model.add(keras.layers.MaxPooling1D(1))
-#model.add(keras.layers.Dropout(0.25))
-'''
-'''
-model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='valid', data_format='channels_last', input_shape=(128,128,3)))
-filters = 20
-kernel_size = 3
-model.add(keras.layers.Conv1D(filters, kernel_size, strides=1, padding='valid', data_format='channels_last', dilation_rate=1, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None))
-#model.add(keras.layers.Conv2D(filters, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None))
-'''
-# use model.add() to add any layers you like
-# read Keras documentation to find which layers you can use:
-#           https://keras.io/layers/core/
-#           https://keras.io/layers/convolutional/
-#           https://keras.io/layers/pooling/
-#
-
-# last layer should be with softmax activation function - do not change!!!
-model.add(layers.Dense(classes, activation='softmax'))
+# this should print your model's structure...
 print(model.summary())
 # fill optimizer argument using one of keras.optimizers.
 # read Keras documentation : https://keras.io/models/model/
@@ -107,6 +69,9 @@ model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
 # you can change number of epochs by changing the value of the 'epochs' paramter
 model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs= 8 , validation_data=valid_gen, validation_steps=v_steps)
-model.save('flowers_model.h5')
+
+model.save(getName() + '.h5')
+# model.save('flowers_model.h5')
+
 plt_modle(model_hist)
 
---
+++ model_rotem_2.py
@@ -0,0 +1,32 @@
+# must use these rows:
+import keras
+from keras import layers
+
+
+# keep the function's name as it is...
+def initializeModel(model, img_size, classes):
+    # first layer, must have: input_shape=(img_size, img_size, 3)
+    model.add(keras.layers.Conv2D(64, kernel_size=(5, 5), padding='same', input_shape=(img_size, img_size, 3), activation='relu'))
+
+    # middle layers - do whatever you want with them:
+    model.add(keras.layers.Conv2D(80, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(300, activation='relu'))
+
+    # use model.add() to add any layers you like
+    # read Keras documentation to find which layers you can use:
+    #           https://keras.io/layers/core/
+    #           https://keras.io/layers/convolutional/
+    #           https://keras.io/layers/pooling/
+    #
+
+    # last layer should be with softmax activation function - do not change!!!
+    model.add(layers.Dense(classes, activation='softmax'))
+
+
+def getName():
+    return 'model_rotem_2'
