8d3f798125c18b0249c2171e8bc7efcece36fe7f
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-04-22 15:16:28 +0300

    new model for stiven to run

--- model_rotem_2.py
+++ model_rotem_2.py
@@ -10,9 +10,12 @@ def initializeModel(model, img_size, classes):
 
     # middle layers - do whatever you want with them:
     model.add(keras.layers.Conv2D(80, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
     model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
-    model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
-    model.add(keras.layers.Conv2D(128, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(keras.layers.Conv2D(256, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(keras.layers.Conv2D(256, kernel_size=(5, 5), padding='same', activation='relu'))
     model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
     model.add(keras.layers.Flatten())
     model.add(keras.layers.Dense(300, activation='relu'))
