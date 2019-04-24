600d7bdbaf2caec8b988fd5166d3ff89c3d79b63
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-04-24 22:31:27 +0300

    prediction works :)

--- flower_classification.py
+++ flower_classification.py
@@ -37,21 +37,56 @@ def plt_modle(model_hist):
 
 
 def load_trained_model(weights_path):
-    global model
-    from model_rotem_1 import initializeModel
+    global model, img_size, classes
+    from model_rotem_12 import initializeModel
     model = models.Sequential()
     initializeModel(model, img_size, classes)
     model.load_weights(weights_path)
-    print(model.summary())
+    #print(model.summary())
 
 
-def predict(test_set):
+def getPredictedValue(prediction):
+    maxProb = -1
+    pred = -1
+    for i in range(len(prediction)):
+        if prediction[i] > maxProb:
+            maxProb = prediction[i]
+            pred = i
+
+    if pred == 0:
+        return 'daisy'
+    elif pred == 1:
+        return 'dandelion'
+    elif pred == 2:
+        return 'rose'
+    elif pred == 3:
+        return 'sunflower'
+    else:  # pred == 4
+        return 'tulip'
+
+
+def predict():
     global model
+    from keras.preprocessing import image
+    import numpy as np
+
+    img_path = 'flowers/rose/110472418_87b6a3aa98_m.jpg'
+    # img = image.load_img(img_path, target_size=(224, 224)) # if a you want a spesific image size
+    img = image.load_img(img_path, target_size = (img_size, img_size))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = x * 1. / 255  # rescale as training
+
+    prediction = model.predict(x)  # Vector with the prob of each class
+
+    print(prediction)
+    print(getPredictedValue(prediction[0]))
+    '''
     c = model.predict_classes(test_set)
 
     for i in range(len(test_set)):
         print("X=%s, Predicted=%s" % (test_set[i], c[i]))
-
+    '''
 
 # Split images into Training and Validation Sets (20%)
 
@@ -67,20 +102,27 @@ train_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_
 valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode = 'categorical', subset='validation')
 
 #test_gen = train.
-#load_trained_model('flowers_model.h5')
-#predict(valid_gen)
+load_trained_model('model_rotem_12_fittingModel13With72epoch.h5')
+predict()
 
 # Model
-
+'''
 model = models.Sequential()
 
 # you can change the 'from' parameter to the file you are using your model on:
 # example, change to from model_<my name>_<my index> import initializeModel
-from model_rotem_10 import initializeModel, getName
+
+from model_rotem_12 import getName
+load_trained_model('model_rotem_12_fittingModel13With72epoch.h5')
+'''
+'''
+from model_rotem_12 import initializeModel, getName
 initializeModel(model, img_size, classes)
+'''
 '''
 from model_rotem_9 import initializeModel, getName
 model = initializeModel(img_size, classes)
+'''
 '''
 # this should print your model's structure...
 print(model.summary())
@@ -96,7 +138,7 @@ from timeit import default_timer as timer
 start = timer()
 
 # you can change number of epochs by changing the value of the 'epochs' paramter
-model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs= 30 , validation_data=valid_gen, validation_steps=v_steps)
+model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs= 8 , validation_data=valid_gen, validation_steps=v_steps)
 
 model.save(getName() + '.h5')
 # model.save('flowers_model.h5')
@@ -107,3 +149,4 @@ end = timer()
 elapsed = end - start # Time in seconds
 print('time in minutes: ')
 print(elapsed / 60)
+'''
