189397e840739ec2669316d1a2ea56674bad60f9
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-04-28 01:09:06 +0300

    predict done, started GUI

--- flower_classification.py
+++ flower_classification.py
@@ -4,6 +4,9 @@ from keras import models
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import sys
+import os
+import pandas as pd
+import numpy as np
 from PIL import Image
 sys.modules['Image'] = Image
 
@@ -36,23 +39,60 @@ def plt_modle(model_hist):
     plt.show()
 
 
+def get_model_structure(img_size, classes):
+    model = models.Sequential()
+
+    model.add(keras.layers.Conv2D(96, kernel_size=(3, 3), padding='same', input_shape=(img_size, img_size, 3), activation='relu'))
+
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(256, kernel_size=(3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(384, kernel_size=(3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(256, kernel_size=(3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(4096, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(4096, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+
+    # use model.add() to add any layers you like
+    # read Keras documentation to find which layers you can use:
+    #           https://keras.io/layers/core/
+    #           https://keras.io/layers/convolutional/
+    #           https://keras.io/layers/pooling/
+    #
+
+    # last layer should be with softmax activation function - do not change!!!
+    model.add(layers.Dense(classes, activation='softmax'))
+
+    return model
+
+
 def load_trained_model(weights_path):
     global model, img_size, classes
-    from model_rotem_12 import initializeModel
-    model = models.Sequential()
-    initializeModel(model, img_size, classes)
+    #from model_rotem_12 import initializeModel
+    #model = models.Sequential()
+    #initializeModel(model, img_size, classes)
+    model = get_model_structure(img_size, classes)
     model.load_weights(weights_path)
     #print(model.summary())
 
 
-def getPredictedValue(prediction):
+def getPredictedValue(pred):
+    '''
     maxProb = -1
     pred = -1
     for i in range(len(prediction)):
         if prediction[i] > maxProb:
             maxProb = prediction[i]
             pred = i
-
+    '''
     if pred == 0:
         return 'daisy'
     elif pred == 1:
@@ -65,22 +105,65 @@ def getPredictedValue(prediction):
         return 'tulip'
 
 
-def predict():
+def predict(path):
     global model
-    from keras.preprocessing import image
-    import numpy as np
+    # from keras.preprocessing import image
 
+    '''
+    test_generator = test_datagen.flow_from_directory(
+    directory=pred_dir,
+    target_size=(28, 28),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode=None,
+    shuffle=False
+    )
+    '''
+    numOfFiles = 0
+    for root, dirs, files in os.walk(path):
+        numOfFiles += len(files)
+
+    test_generator = train.flow_from_directory(
+        directory=path,
+        target_size=(img_size, img_size),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False
+    )
+    test_generator.reset()
+
+    pred = model.predict_generator(test_generator, verbose=1, steps=numOfFiles / batch_size)
+
+    predicted_class_indices = np.argmax(pred, axis=1)
+    '''
+    labels = test_generator.class_indices
+    labels = dict((v, k) for k, v in labels.items())
+    predictions = [labels[k] for k in predicted_class_indices]
+    '''
+    predictions = [getPredictedValue(k) for k in predicted_class_indices]
+
+    filenames = test_generator.filenames
+    results = pd.DataFrame({"Filename": filenames, "Predictions": predictions})
+
+    results.to_csv("output.csv", sep=',', index=False)
+
+    '''
     img_path = 'flowers/rose/110472418_87b6a3aa98_m.jpg'
     # img = image.load_img(img_path, target_size=(224, 224)) # if a you want a spesific image size
     img = image.load_img(img_path, target_size = (img_size, img_size))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = x * 1. / 255  # rescale as training
-
-    prediction = model.predict(x)  # Vector with the prob of each class
-
+    '''
+    '''
+    test_gen = train.flow_from_directory(path, target_size=(img_size, img_size), batch_size=batch_size, class_mode='categorical')
+    to_test = np.asarray(test_gen)
+    print("hello")
+    prediction = model.predict(to_test)  # Vector with the prob of each class
+    print("hello from the other side")
     print(prediction)
     print(getPredictedValue(prediction[0]))
+    '''
     '''
     c = model.predict_classes(test_set)
 
@@ -90,6 +173,7 @@ def predict():
 
 # Split images into Training and Validation Sets (20%)
 
+
 train = ImageDataGenerator(rescale=1./255,horizontal_flip=True, shear_range=0.2, zoom_range=0.2,width_shift_range=0.2,height_shift_range=0.2, fill_mode='nearest', validation_split=0.2)
 
 img_size = 128
@@ -98,20 +182,23 @@ t_steps = 3462/batch_size
 v_steps = 861/batch_size
 classes = 5
 flower_path = "flowers"
+'''
 train_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode='categorical', subset='training')
 valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode = 'categorical', subset='validation')
-
+'''
+'''
 #test_gen = train.
 load_trained_model('model_rotem_12_fittingModel13With72epoch.h5')
-predict()
-
+predict("C:\\Users\\Rotem\\Desktop\\תואר ראשון\\הכנה לפרוייקט\\עבודה 2\\flowers")
+'''
 # Model
 '''
 model = models.Sequential()
 
 # you can change the 'from' parameter to the file you are using your model on:
 # example, change to from model_<my name>_<my index> import initializeModel
-
+'''
+'''
 from model_rotem_12 import getName
 load_trained_model('model_rotem_12_fittingModel13With72epoch.h5')
 '''
--- gui.py
+++ gui.py
@@ -1,5 +1,15 @@
 from tkinter import *
 from tkinter import filedialog
+from flower_classification import get_model_structure
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import os
+import pandas as pd
+import numpy as np
+from PyQt5.QtWidgets import QWidget,QScrollArea, QTableWidget, QVBoxLayout,QTableWidgetItem
+
+img_size = 128
+batch_size = 20
+classes = 5
 
 root = Tk()
 root.title("Flower Classification")
@@ -14,9 +24,9 @@ classifyPathEntry.grid(row = 4, column = 1, sticky = W)
 
 def browse_classify_folders():
     global folder_path
-    filename = filedialog.askdirectory()
+    folder_path = filedialog.askdirectory()
     classifyPathEntry.delete(0, END)
-    classifyPathEntry.insert(END, filename)
+    classifyPathEntry.insert(END, folder_path)
 
 
 chooseClassifyFolder = Button(root, text="Browse", width = 6, command = browse_classify_folders)
@@ -29,19 +39,113 @@ modelPathEntry = Entry(root, width=40, bg="white")
 modelPathEntry.grid(row = 5, column = 1, sticky = W)
 
 
+def load_trained_model(weights_path):
+    global model, img_size, classes
+    model = get_model_structure(img_size, classes)
+    model.load_weights(weights_path)
+    # print(model.summary())
+
+
 def browse_model_folders():
-    global folder_path
-    filename = filedialog.askdirectory()
+    global model_filename
+    model_filename = filedialog.askopenfile().name
     modelPathEntry.delete(0, END)
-    modelPathEntry.insert(END, filename)
+    modelPathEntry.insert(END, model_filename)
+    load_trained_model(model_filename)
 
 
 chooseModelFolder = Button(root, text="Browse", width = 6, command = browse_model_folders)
 chooseModelFolder.grid(row = 5, column = 2, sticky = W)
 
 
+def getPredictedValue(pred):
+    if pred == 0:
+        return 'daisy'
+    elif pred == 1:
+        return 'dandelion'
+    elif pred == 2:
+        return 'rose'
+    elif pred == 3:
+        return 'sunflower'
+    else:  # pred == 4
+        return 'tulip'
+
+
+def predict(path_from, path_to):
+    global model
+
+    numOfFiles = 0
+    for root, dirs, files in os.walk(path_from):
+        numOfFiles += len(files)
+
+    train = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True, shear_range=0.2, zoom_range=0.2, width_shift_range=0.2, height_shift_range=0.2, fill_mode='nearest', validation_split=0.2)
+
+    test_generator = train.flow_from_directory(
+        directory=path_from,
+        target_size=(img_size, img_size),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False
+    )
+    test_generator.reset()
+
+    pred = model.predict_generator(test_generator, verbose=1, steps=numOfFiles / batch_size)
+
+    predicted_class_indices = np.argmax(pred, axis=1)
+    predictions = [getPredictedValue(k) for k in predicted_class_indices]
+
+    filenames = test_generator.filenames
+    results = pd.DataFrame({"Filename": filenames, "Predictions": predictions})
+
+    results.to_csv(path_to, sep=',', index=False)
+    return results
+
+
+def show_prediction(results):
+    '''
+    win = QWidget()
+    scroll = QScrollArea()
+    layout = QVBoxLayout()
+    table = QTableWidget()
+    scroll.setWidget(table)
+    layout.addWidget(table)
+    win.setLayout(layout)
+
+    table.setColumnCount(len(results.columns))
+    table.setRowCount(len(results.index))
+    for i in range(len(results.index)):
+        for j in range(len(results.columns)):
+            table.setItem(i, j, QTableWidgetItem(str(results.iloc[i, j])))
+
+    win.show()
+    '''
+    #popup = Tk()
+    #popup.wm_title("!")
+    import plotly.plotly as py
+    import plotly.graph_objs as go
+
+    df = results
+
+    trace = go.Table(
+        header=dict(values=list(df.columns),
+                    fill=dict(color='#C2D4FF'),
+                    align=['left'] * 2),
+        cells=dict(values=[df.Filename, df.Predictions],
+                   fill=dict(color='#F5F8FF'),
+                   align=['left'] * 2))
+
+    data = [trace]
+    py.iplot(data, filename='results')
+    #popup.mainloop()
+
+
 def make_prediction():
-    pass
+    global folder_path
+    path_to = filedialog.asksaveasfile(mode='w', defaultextension=".csv")
+    if path_to is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    results = predict(folder_path, path_to.name)
+    show_prediction(results)
 
 
 predictBtn = Button(root, text="Predict", width = 7, command = make_prediction)
